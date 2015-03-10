3bfcd3ab790b897531f40306d749bced81853003
NathiMi <nathi.xx@web.de>
2015-03-10 16:29:21 +0100

    Startscene2.2

---
+++ Assets/Scripts/Start2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartEntführung : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("escape")){
+			Application.Quit ();
+		}
+	}
+
+	void OnMouseDown () {
+		Application.LoadLevel ("Start");
+	}
+}
