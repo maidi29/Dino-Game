c098c98fdad9e4dd8296329cbec733f9e0f48d29
KseniaKi <ksenia.kirichenko@web.de>
2015-03-15 22:25:15 +0100

    sound2

---
+++ Assets/Scripts/walkingwatersound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walkingwatersound : MonoBehaviour {
+
+	GameObject dino;
+	bool dinocol;
+
+	// Use this for initialization
+	void Start () {
+
+		dino = GameObject.Find ("Dino");
+		dinocol = false;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dinocol == true)
+			audio.Play ();
+	
+	}
+	void OnTrigger (Collider collid)
+	{
+		if (collid.gameObject == dino)
+		{
+			dinocol = true;
+ }
+}
+}
